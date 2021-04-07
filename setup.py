2a0af2116bb52d79cede9c2205e9ce6806c006af
Peter-32 <peter@impactradius.com>
2021-04-07 14:44:58 -0700

    update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'dscoe_utils',         # How you named your package folder (MyLib)
   packages = ['dscoe_utils'],   # Chose the same as "name"
-  version = '0.1',      # Start with a small number and increase it with every change you make
+  version = '0.2',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'dscoe_utils',   # Give a short description about your library
   author = 'ir-datascience',                   # Type in your name
