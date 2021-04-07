40b264b0be594c3450253e5dc0ac2b098960b4fa
Peter-32 <peter@impactradius.com>
2021-04-07 14:51:14 -0700

    update

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'dscoe_utils',         # How you named your package folder (MyLib)
   packages = ['dscoe_utils'],   # Chose the same as "name"
-  version = '0.2',      # Start with a small number and increase it with every change you make
+  version = '0.3',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'dscoe_utils',   # Give a short description about your library
   author = 'ir-datascience',                   # Type in your name
   author_email = 'peter@impact.com',      # Type in your E-Mail
   url = 'https://github.com/Peter-32/dscoe_utils',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/Peter-32/dscoe_utils/archive/refs/tags/v_02.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/Peter-32/dscoe_utils/archive/refs/tags/v_03.tar.gz',    # I explain this later on
   keywords = ['dsutils'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
       ],
